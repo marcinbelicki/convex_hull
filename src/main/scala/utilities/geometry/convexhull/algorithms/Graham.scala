135d784612bd2f2df20c877f4199a1d510f22286
marcin.belicki <marcin.belicki@abaro.net>
2023-04-15 09:37:24 +0200

    proper English

--- src/main/scala/utilities/geometry/convexhull/algorithms/Graham.scala
+++ src/main/scala/utilities/geometry/convexhull/algorithms/Graham.scala
@@ -7,7 +7,7 @@ import utilities.geometry.{PointsCycle, PointsUtils}
 case object Graham extends ConvexHullAlgorithm {
 
   override def calculate(points: Points)(implicit ordering: OrientationOrdering): Points = {
-    require(points.nonEmpty, "You can not calculate convex hull of a empty points set.")
+    require(points.nonEmpty, "You can not calculate convex hull of an empty points set.")
     val centroid = PointsUtils.calculateCentroid(points)
     val pointsSorted = points.map(_ - centroid).sorted
     val pointsCycle = new PointsCycle(pointsSorted)
